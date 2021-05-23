3ab6d374837850b56d62be18d83ffb571f65344e
Oleg Lihogub <oligli@yandex.ru>
2021-05-23 23:00:15 +0300

    universal crossingover implemented

--- src/ga/crossingover/UniversalCrossingover.kt
+++ src/ga/crossingover/UniversalCrossingover.kt
@@ -1,9 +1,17 @@
 package ga.crossingover
 
 import ga.chromosome.Chromosome
+import ga.chromosome.ChromosomeImpl
+import kotlin.random.Random
 
 class UniversalCrossingover : Crossingover() {
     override fun crossingover(chromosomeA: Chromosome, chromosomeB: Chromosome): Chromosome {
-        TODO("Not yet implemented")
+        val chromosomeLength = chromosomeA.getLength()
+        val newChromosome = ChromosomeImpl()
+        for (i in 0 until chromosomeLength) {
+            val gene = if (Random.nextBoolean()) chromosomeA.getGene(i) else chromosomeB.getGene(i)
+            newChromosome.setGene(i, gene)
+        }
+        return newChromosome
     }
 }
