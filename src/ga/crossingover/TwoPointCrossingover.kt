ba261581b72143dff32fdb3bec12cca6d7869541
Oleg Lihogub <oligli@yandex.ru>
2021-05-23 23:00:35 +0300

    two point crossingover implemented

--- src/ga/crossingover/TwoPointCrossingover.kt
+++ src/ga/crossingover/TwoPointCrossingover.kt
@@ -1,9 +1,19 @@
 package ga.crossingover
 
 import ga.chromosome.Chromosome
+import ga.chromosome.ChromosomeImpl
+import kotlin.random.Random
 
 class TwoPointCrossingover : Crossingover() {
     override fun crossingover(chromosomeA: Chromosome, chromosomeB: Chromosome): Chromosome {
-        TODO("Not yet implemented")
+        val chromosomeLength = chromosomeA.getLength()
+        val firstPoint = Random.nextInt(chromosomeLength)
+        val secondPoint = Random.nextInt(firstPoint, chromosomeLength)
+        val newChromosome = ChromosomeImpl(chromosomeA)
+        for (i in firstPoint until secondPoint) {
+            val gene = chromosomeB.getGene(i)
+            newChromosome.setGene(i, gene)
+        }
+        return newChromosome
     }
 }
