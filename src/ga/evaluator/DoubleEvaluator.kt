b05a9ffc3bee8968a77ea74eb1f3b10deb4973b8
Oleg Lihogub <oligli@yandex.ru>
2021-06-04 22:50:25 +0300

    added median evaluation

--- src/ga/evaluator/DoubleEvaluator.kt
+++ src/ga/evaluator/DoubleEvaluator.kt
@@ -6,13 +6,15 @@ import kotlin.math.pow
 class DoubleEvaluator(private val targetFunction: (Double) -> Double,
                       private val leftBorder: Double,
                       private val rightBorder: Double) : Evaluator() {
-    override fun evaluate(chromosome: Chromosome): Double {
+    override fun toValue(chromosome: Chromosome): Double {
         var value = 0.0
         val genome = chromosome.getGenome()
         Array(genome.size) {i -> i}
             .forEach { i -> if (genome[i].getState()) value += 2.0.pow(i.toDouble()) }
-        return targetFunction(leftBorder + (rightBorder - leftBorder)*(value/2.0.pow(32)))
+        return leftBorder + (rightBorder - leftBorder)*(value/2.0.pow(32))
     }
+
+    override fun evaluate(chromosome: Chromosome): Double = targetFunction(this.toValue(chromosome))
 
     override fun average(population: List<Chromosome>): Double {
         return population.map {c -> evaluate(c)}.average()
@@ -24,5 +26,9 @@ class DoubleEvaluator(private val targetFunction: (Double) -> Double,
 
     override fun max(population: List<Chromosome>): Double {
         return population.map {c -> evaluate(c)}.maxOrNull() ?: Double.MAX_VALUE
+    }
+
+    override fun median(population: List<Chromosome>): Double {
+        return evaluate(population.sortedBy { c -> evaluate(c) }[population.size/2])
     }
 }
--- src/ga/evaluator/Evaluator.kt
+++ src/ga/evaluator/Evaluator.kt
@@ -3,8 +3,10 @@ package ga.evaluator
 import ga.chromosome.Chromosome
 
 abstract class Evaluator {
+    abstract fun toValue(chromosome: Chromosome): Double
     abstract fun evaluate(chromosome: Chromosome): Double
     abstract fun average(population: List<Chromosome>): Double
     abstract fun min(population: List<Chromosome>): Double
     abstract fun max(population: List<Chromosome>): Double
+    abstract fun median(population: List<Chromosome>): Double
 }
